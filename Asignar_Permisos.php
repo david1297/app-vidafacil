a9463081289deb91d31a7c179a74c2855c29b925
david1297 <juandavid.andrade1997@gmail.com>
2020-04-20 15:30:03 -0500

    .

--- Asignar_Permisos.php
+++ Asignar_Permisos.php
@@ -3,21 +3,18 @@ include('is_logged.php');
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
     exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
-    require_once("../../libraries/password_compatibility_library.php");
+    require_once("libraries/password_compatibility_library.php");
 }		
 
-            require_once ("../../config/db.php");
-			require_once ("../../config/conexion.php");
-			$sql=mysqli_query($con, "select Nit from USUARIOS ");
+            require_once ("config/db.php");
+			require_once ("config/conexion.php");
+			$sql=mysqli_query($con, "select Nit from USUARIOS where Rol =2 ");
 	while($row=mysqli_fetch_array($sql)){
 		$Nit=$row["Nit"];
-		$sql =  "INSERT INTO PERMISOS (Modulo, Permiso, Estado,Usuario, Descripcion) VALUES 
-						
-						('Afiliados', 'ConsultarTodoA', 'false', '".$Nit."', 'Ver Todo Agendamientos');
-						
-						
-						;";
-							$query_update = mysqli_query($con,$sql);
+		$sql1 =  "INSERT INTO PERMISOS (Modulo, Permiso, Estado,Usuario, Descripcion) VALUES 
+		('Afiliados', 'ConsultarTodoA', 'false', '".$Nit."', 'Ver Todo Agendamientos');";
+		echo $sql1.'<br>';			
+							$query_update = mysqli_query($con,$sql1);
 		  
 	}
 
